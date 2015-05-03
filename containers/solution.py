40b3f0a6d0b1c2919675a32fdfd59f49469e845c
Makarov, Viacheslav <makarov.viacheslav@gmail.com>
2015-05-03 16:58:13 +0300

    Beginning containers.

---
+++ containers/solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def testEqual(actualResult, expectedResult):
+   if actualResult == expectedResult:
+      print "passed"
+   else:
+      print "failed, expected " + str(expectedResult) + ", returned " + str(actualResult)
+
+#
+
